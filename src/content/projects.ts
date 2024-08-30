a024ce16259cbf730706ebeb74eafb8f82752039
akinloluwami <akinkunmioye42@gmail.com>
2024-08-30 01:49:31 +0100

    Projects list

---
+++ src/content/projects.ts
@@ -0,0 +1,39 @@
+interface Props {
+  name: string;
+  description: string;
+  website?: string;
+  github?: string;
+  tags?: string[];
+  status?: string;
+}
+
+export const projects: Props[] = [
+  {
+    name: "Sooner",
+    description: "Codetime tracking for developers and software teams.",
+    website: "https://www.sooner.run",
+    github: "https://github.com/sooner-run/sooner",
+    tags: ["TypeScript", "Next.js", "Hono", "DrizzleORM", "TailwindCSS"],
+  },
+  {
+    name: "UploadFly",
+    description: "Really simple file uploads infrastructure.",
+    website: "https://www.uploadfly.co",
+    github: "https://github.com/sooner-run/sooner",
+    tags: ["TypeScript", "S3", "Next.js", "TailwindCSS"],
+  },
+  {
+    name: "LogDrop",
+    description: "API analytics, logging, monitoring and alerts for NodeJS.",
+    website: "https://www.sooner.run",
+    github: "https://github.com/akinkloluwami/logdrop",
+    tags: ["TypeScript", "Next.js", "TailwindCSS"],
+  },
+  {
+    name: "Keydown",
+    description: "Minimalist blogging engine.",
+    website: "https://keydown-blog.vercel.app",
+    tags: ["TypeScript", "Next.js", "TailwindCSS"],
+    status: "private + wip",
+  },
+];
