ca4d02630fb50cb08f60f8abfc0952c4079ed488
titanium-dev <akinkunmioye42@gmail.com>
2025-08-23 23:46:57 +0100

    style: enhance Header component by adjusting image size and updating quote styling

--- src/components/react/header.tsx
+++ src/components/react/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   Hello, I am
   <span className='flex items-center space-x-4 ml-2'>
     Akinkunmi.
-    <img src="/me.png" alt="Akinkunmi" className='w-10 rotate-6 ml-2 rounded-md'/>
+    <img src="/me.png" alt="Akinkunmi" className='w-14 rotate-6 ml-2 rounded-md hover:-rotate-6 transition-all duration-500 ease-in-out'/>
   </span>
 </h1>
         <div className="mt-10 space-y-6 text-lg font-light">
@@ -28,7 +28,7 @@ const Header = () => {
             </span>
              {" "}ecosystem, but I'm also interested in other languages such as Elixir, Rust, and Go.</p>
 
-  <p className='italic'>
+  <p className='quote'>
              My goal is to be as cracked of an engineer as I can be.
          </p>
 
