027467346c5fd71b7f4c46420c62bd4b7a52abd8
titanium-dev <akinkunmioye42@gmail.com>
2025-08-26 21:52:29 +0100

    style: update Header component layout and quote formatting for improved presentation

--- src/components/react/header.tsx
+++ src/components/react/header.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import {ArrowUpRight} from 'lucide-react'
 
 const Header = () => {
+
+    const links = [{
+        title: "GitHub",
+        url: "https://github.com/akinloluwami"
+    }, {
+        title: "Mail",
+        url:"mailto:akinkunmioye42@gmail.com"
+    }, {
+        title: "Twitter",
+        url: "https://twitter.com/xing_titanium"
+    }]
+
   return (
     <div>
 <h1 className='text-3xl mt-20 flex items-center space-x-4'>
   Hello, I am
   <span className='flex items-center space-x-4 ml-2'>
     Akinkunmi.
-    <img src="/me.png" alt="Akinkunmi" className='w-14 rotate-6 ml-2 rounded-md hover:-rotate-6 transition-all duration-500 ease-in-out'/>
+    <img src="/me.png" alt="Akinkunmi" className='w-10 rotate-6 ml-2 rounded-md hover:-rotate-6 transition-all duration-500 ease-in-out'/>
   </span>
 </h1>
         <div className="mt-10 space-y-6 text-lg font-light">
@@ -28,10 +40,18 @@ const Header = () => {
             </span>
              {" "}ecosystem, but I'm also interested in other languages such as Elixir, Rust, and Go.</p>
 
-  <p className='quote'>
-             My goal is to be as cracked of an engineer as I can be.
+  <p className=''>
+             <q>My goal is to be as cracked of an engineer as I can be.</q>
          </p>
-
+ 
+        </div>
+        <div className="flex items-center space-x-4 mt-10">
+          {links.map(link => (
+            <a key={link.title} href={link.url} target='_blank' className="bg-white/5 px-4 py-2 rounded-full hover:bg-white/10 transition-all duration-300 flex items-center gap-x-2">
+                <ArrowUpRight />
+              {link.title}
+            </a>
+          ))}
         </div>
     </div>
   )
