5182e49d1af0ce810accb86b1dd8923bc0571431
Akinkunmi <akinkunmioye42@gmail.com>
2026-02-11 09:01:41 +0100

    feat: add Vercel adapter and auth integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  output: "server",
+  adapter: vercel(),
+  integrations: [tailwind(), react(), auth()],
 });
